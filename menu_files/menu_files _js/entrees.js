8fe278e2b437e2d95ea811da75e5b247799523a8
Saurabhpankaj <Saurabhpankaj786@gmail.com>
2023-07-15 11:06:42 +0530

    1

---
+++ menu_files/menu_files _js/entrees.js
@@ -0,0 +1,78 @@
+const savedData = JSON.parse(localStorage.getItem("site_data"));
+
+
+ function renderdata(data) {
+ let newdata = data.map((el) =>{
+    return {
+        image: el.image,
+        title: el.title,
+        price: Math.floor(Math.random() * 500)
+    }
+ })
+
+    let cardlist = `
+<div class="card-list">
+${newdata
+            .map((item) =>
+                getcard(
+                    item.image,
+                    item.title,
+                    item.desc?item.desc:"Chick-fil-A Chick-n-Minis™",
+                    item.price
+                )
+            )
+            .join('')}
+</div>
+`;
+
+    let main_file = document.getElementById('main_div')
+    
+    main_file.innerHTML = null
+    main_file.innerHTML = cardlist
+
+
+}
+setTimeout(() => {
+    renderdata(savedData.entrees)
+}, 1000);
+
+
+function getcard(image, title, desc,price) {
+    let card = `
+    <div class="child_div1">
+    <img src=${image}>
+    <a href=""  class="title">${title}</a>
+    <p class="nutrients">${desc}</p>
+    <button  onclick="cardlistt('${title}', '${image}','${price}')" class="child_div_btn">Order now</button>
+           </div>
+           `;
+           
+    return card
+}
+let arr=[]
+function cardlistt(title, image, price) {
+    
+    let existingData = JSON.parse(localStorage.getItem('cardData')) || [];
+  
+    
+    let itemExists = existingData.some((item) => item.title === title);
+  
+    if (itemExists) {
+     
+      alert(`item is already in cart. Please make changes if needed.`);
+    } else {
+      
+      let newData = {
+        image: image,
+        title: title,
+        price: price
+      };
+      existingData.push(newData);
+  
+      
+      localStorage.setItem('cardData', JSON.stringify(existingData));
+  
+      
+      alert(`item added to cart!`);
+    }
+  }
---
+++ scriptfile/index.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+let arr=['https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/Daypart%20Hero/Winter23/Winter23-DotCom-D_710x580-2.1.jpg?h=580&w=710&la=en',
+'https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/Daypart%20Hero/large-CFA2212020-Catering-Daypart-Desktop_710x580V3-1.jpg?h=580&w=710&la=en',
+'https://st3.depositphotos.com/1000504/17303/i/450/depositphotos_173037454-stock-photo-fresh-tasty-burger.jpg',
+         'https://img.freepik.com/premium-photo/cheese-burger-with-onion-tomato-lettuce-bacon-white-background_499484-1161.jpg?w=900',
+        'https://img.freepik.com/premium-photo/divided-hamburger_147620-1303.jpg?w=740']
+  let hederArray=[`Partners in deliciousness`,`Make it a milkshake break`,`It’ll be our pleasure to serve you again soon`]
+        let count=0;
+        let count1=0
+        function changeImage()
+        {
+
+                   let hederslid=document.querySelector('#heder')
+                   hederslid.innertext=hederArray[count1]
+                      let img=document.querySelector('#sliding')
+                      img.setAttribute("src",arr[count])
+                       count++
+                       count1++
+                       if(count==4)
+                         {
+                          count=0
+                         }
+                         if(count1==2)
+                         {
+                            count1=0
+                         }
+                      }
+      
+      setInterval(changeImage,5000)
+
+
+
+      
